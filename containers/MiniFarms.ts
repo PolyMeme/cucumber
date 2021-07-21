dbd02435e868701fa6bc7271d67284a0ce73d084
PolyMeme <0xpolymeme@gmail.com>
2021-07-21 00:02:30 -0400

    updated pickle icons

--- containers/MiniFarms.ts
+++ containers/MiniFarms.ts
@@ -11,50 +11,50 @@ interface IFarmInfo {
 }
 
 export const FarmInfo: IFarmInfo = {
-  "0x9eD7e3590F2fB9EEE382dfC55c71F9d3DF12556c": {
-    tokenName: "pCLP USDC/WETH",
-    poolName: "pCLP USDC/WETH",
-  },
-  "0x7512105DBb4C0E0432844070a45B7EA0D83a23fD": {
-    tokenName: "pCLP PICKLE/MUST",
-    poolName: "pCLP PICKLE/MUST",
-  },
-  "0x91bcc0BBC2ecA760e3b8A79903CbA53483A7012C": {
-    tokenName: "pCLP MATIC/MUST",
-    poolName: "pCLP MATIC/MUST",
-  },
-  "0x0519848e57Ba0469AA5275283ec0712c91e20D8E": {
-    tokenName: "pAaveDAI",
-    poolName: "pAaveDAI",
-  },
-  "0x261b5619d85B710f1c2570b65ee945975E2cC221": {
+  // "0x9eD7e3590F2fB9EEE382dfC55c71F9d3DF12556c": {
+  //   tokenName: "pCLP USDC/WETH",
+  //   poolName: "pCLP USDC/WETH",
+  // },
+  // "0x7512105DBb4C0E0432844070a45B7EA0D83a23fD": {
+  //   tokenName: "pCLP PICKLE/MUST",
+  //   poolName: "pCLP PICKLE/MUST",
+  // },
+  // "0x91bcc0BBC2ecA760e3b8A79903CbA53483A7012C": {
+  //   tokenName: "pCLP MATIC/MUST",
+  //   poolName: "pCLP MATIC/MUST",
+  // },
+  // "0x0519848e57Ba0469AA5275283ec0712c91e20D8E": {
+  //   tokenName: "pAaveDAI",
+  //   poolName: "pAaveDAI",
+  // },
+  "0xdbaf35cd931409a1209b0d075a723878d383b0e3": {
     tokenName: "am3CRV",
     poolName: "am3CRV",
   },
-  "0x80aB65b1525816Ffe4222607EDa73F86D211AC95": {
-    tokenName: "pSLP ETH/USDT",
-    poolName: "pSLP ETH/USDT",
-  },
-  "0xd438Ba7217240a378238AcE3f44EFaaaF8aaC75A": {
-    tokenName: "pSLP ETH/MATIC",
-    poolName: "pSLP ETH/MATIC",
-  },
-  "0xf12BB9dcD40201b5A110e11E38DcddF4d11E6f83": {
-    tokenName: "pQLP MAI (old)",
-    poolName: "pQLP MAI (old)",
-  },
-  "0x74dC9cdCa9a96Fd0B7900e6eb953d1EA8567c3Ce": {
-    tokenName: "pQLP MAI",
-    poolName: "pQLP MAI",
-  },
-  "0xd06a56c864C80e4cC76A2eF778183104BF0c848d": {
-    tokenName: "pQLP QI",
-    poolName: "pQLP QI",
-  },
-  "0xE484Ed97E19F6B649E78db0F37D173C392F7A1D9": {
-    tokenName: "IS3USD",
-    poolName: "IS3USD",
-  }
+  // "0x80aB65b1525816Ffe4222607EDa73F86D211AC95": {
+  //   tokenName: "pSLP ETH/USDT",
+  //   poolName: "pSLP ETH/USDT",
+  // },
+  // "0xd438Ba7217240a378238AcE3f44EFaaaF8aaC75A": {
+  //   tokenName: "pSLP ETH/MATIC",
+  //   poolName: "pSLP ETH/MATIC",
+  // },
+  // "0xf12BB9dcD40201b5A110e11E38DcddF4d11E6f83": {
+  //   tokenName: "pQLP MAI (old)",
+  //   poolName: "pQLP MAI (old)",
+  // },
+  // "0x74dC9cdCa9a96Fd0B7900e6eb953d1EA8567c3Ce": {
+  //   tokenName: "pQLP MAI",
+  //   poolName: "pQLP MAI",
+  // },
+  // "0xd06a56c864C80e4cC76A2eF778183104BF0c848d": {
+  //   tokenName: "pQLP QI",
+  //   poolName: "pQLP QI",
+  // },
+  // "0xE484Ed97E19F6B649E78db0F37D173C392F7A1D9": {
+  //   tokenName: "IS3USD",
+  //   poolName: "IS3USD",
+  // }
 };
 
 function useFarms() {
